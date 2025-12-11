635c627a1988bca4ff4648da368e6a98934c6d4a
sptcnl <kimmaoo11@naver.com>
2025-12-11 16:29:20 +0900

    경로 수정

--- back/config/settings/base.py
+++ back/config/settings/base.py
@@ -91,7 +91,7 @@ DATABASES = {
     }
 }
 
-AUTH_USER_MODEL = "accounts.User"
+AUTH_USER_MODEL = "apps.accounts.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
