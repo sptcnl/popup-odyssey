562c83e67df66704711a6f4e2172e19e91fb1c42
sptcnl <kimmaoo11@naver.com>
2025-12-12 09:33:54 +0900

    urls에 places, routes, users 추가

--- back/config/urls.py
+++ back/config/urls.py
@@ -20,4 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('apps.accounts.urls')),
+    path('api/places/', include('apps.places.urls')),
+    path('api/routes/', include('apps.routes.urls')),
+    path('api/users/', include('apps.users.urls')),
 ]
