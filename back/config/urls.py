d4340a0459383b04f9764ac883b61998e4ae6243
sptcnl <kimmaoo11@naver.com>
2025-12-15 11:01:16 +0900

    api문서 추가

--- back/config/urls.py
+++ back/config/urls.py
@@ -16,9 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView, 
+    SpectacularSwaggerView, 
+    SpectacularRedocView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     # API 문서
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    # Router URLs
     path('api/accounts/', include('apps.accounts.urls')),
     path('api/places/', include('apps.places.urls')),
     path('api/routes/', include('apps.routes.urls')),
