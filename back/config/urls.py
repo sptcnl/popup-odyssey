57b0ba26e5646b23d14f92621d7656a7e38c7397
nyong <sptcnl@gmail.com>
2026-01-10 11:10:26 +0900

    django initial commit

--- back/config/urls.py
+++ back/config/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/accounts/', include('apps.accounts.urls')),
+    path('api/places/', include('apps.places.urls')),
+    path('api/routes/', include('apps.routes.urls')),
+    path('api/users/', include('apps.users.urls')),
 ]
