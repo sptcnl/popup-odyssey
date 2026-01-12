797ab1a8c8d8ce945c330069f9cf2d405485de1d
nyong <sptcnl@gmail.com>
2026-01-12 16:13:28 +0900

    feat: 회원가입, 로그인, 토큰 리프래쉬 추가

--- back/apps/accounts/urls.py
+++ back/apps/accounts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    
+    path('signup/', views.SignupView.as_view()),
+    path('login/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
 ]
--- back/apps/accounts/views.py
+++ back/apps/accounts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+import re
+from rest_framework import generics
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import AllowAny
+from .serializers import (
+                            SignupSerializer
+                        )
+from rest_framework.exceptions import ValidationError
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+EMAIL_REGEX = re.compile(
+    r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+)
+
+def is_valid_email(email: str) -> bool:
+    return bool(EMAIL_REGEX.match(email))
+
+
+@permission_classes([AllowAny])
+class SignupView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = SignupSerializer
+
+    def perform_create(self, serializer):
+        try:
+            serializer.save()
+        except Exception as e:
+            raise ValidationError({"detail": f"An error occurred: {str(e)}"})
