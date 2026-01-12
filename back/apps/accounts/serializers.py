7cc436fece7e94024a39f44a7fba4c7f87d58723
nyong <sptcnl@gmail.com>
2026-01-12 16:43:56 +0900

    feat: 회원가입 시리얼라이저 추가

---
+++ back/apps/accounts/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class SignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
