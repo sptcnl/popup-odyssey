b0057b8512f7f9681bd4b472ddd222d0c2e8ec46
nyong <sptcnl@gmail.com>
2026-01-17 10:07:23 +0900

    feat: place list create 기능 추가

--- back/apps/places/urls.py
+++ back/apps/places/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import PlaceListCreateAPIView
 
 urlpatterns = [
-    
+    path("", PlaceListCreateAPIView.as_view(), name="place-list-create"),
 ]
--- back/apps/places/views.py
+++ back/apps/places/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# places/views.py
+from rest_framework import generics
+from .models import Place
+from .serializers import PlaceSerializer
 
-# Create your views here.
+
+class PlaceListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Place.objects.all()
+    serializer_class = PlaceSerializer
+
+    # 예: 로그인 유저만 생성자로 넣고 싶으면 override 가능
+    def perform_create(self, serializer):
+        # user 필드가 null 허용이라면 다음은 선택사항
+        if self.request.user.is_authenticated:
+            serializer.save(user=self.request.user)
+        else:
+            serializer.save()
