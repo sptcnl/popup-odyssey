33545ddab21a070c49430165b070f307ae91a34d
nyong <sptcnl@gmail.com>
2026-01-28 15:29:09 +0900

    fix: 환경변수로 변경

--- front/src/api/stores.js
+++ front/src/api/stores.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:8000/api';
+const API_BASE = import.meta.env.VITE_API_BASE;
 
 export const storesApi = {
   async createPlace({ name, address, isPopup, start_date, end_date, is_public = true }) {
