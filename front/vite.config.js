635d67a5f9bce8da6be4f8cb58a8c42352c58e07
nyong <sptcnl@gmail.com>
2026-01-28 15:38:24 +0900

    wip: 라우팅 문제 해결중

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // MIME 타입 및 SPA 라우팅 문제 해결
+  server: {
+    mimeTypes: {
+      'js': 'application/javascript',
+      'mjs': 'application/javascript'
+    }
+  },
+  build: {
+    // Netlify 배포용
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  base: './'
 })
